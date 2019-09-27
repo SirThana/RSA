51586d1f39aa08abcdc296d42bfb51be259ddc4a
SirThana <martijn.zijl.3000@gmail.com>
2019-09-27 20:12:08 +0200

    cleaning up some code

--- rsa.py
+++ rsa.py
@@ -83,61 +83,73 @@ def isPrime(x):
 #   --> Find the relative primes for e and d, using the two large prime numbers
 #       Returns a list of (most) relative numbers, some don't work!, test these by finding
 #       if they rule out 1 with dCheck
-def findCoPrime(p, q):
-    #pubKey = [x,n]
-    n = p * q
-    oN = ((p - 1) * (q - 1))
-    magicNumbers = []
+def RSA(p, q):
+    n = p * q #Calculate the product of two primes
+    oN = ((p - 1) * (q - 1)) #used for eulers theorem || range of common dividers
+    relativePrimes = [] #List of numbers to test for eulers theorem
 
-    #Fix this retarded structure asap please
+    #Fix this retarded structure asap please || finds any relative primes
+    #Appends the relative numbers to a list called relativePrimes
     for e in range (2, oN):
-        isFalse = []
+        checkList = []
         result = e / n
         if result.is_integer() == True:
-            isFalse.append(False)
+            checkList.append(False)
         else:
-            isFalse.append(True)
+            checkList.append(True)
 
         result = e / p
         if result.is_integer() == True:
-            isFalse.append(False)
+            checkList.append(False)
         else:
-            isFalse.append(True)
+            checkList.append(True)
 
         result = e / q
         if result.is_integer() == True:
-            isFalse.append(False)
+            checkList.append(False)
         else:
-            isFalse.append(True)
+            checkList.append(True)
 
         result = oN / e
         if result.is_integer() == True:
-            isFalse.append(False)
+            checkList.append(False)
         else:
-            isFalse.append(True)
+            checkList.append(True)
 
-        if isFalse[0] == True and isFalse[1] == True and isFalse[2] == True and isFalse[3] == True:
-            magicNumbers.append(e)
-            #print(e)
+        if checkList[0] == True and checkList[1] == True and checkList[2] == True and checkList[3] == True:
+            relativePrimes.append(e)
 
-    return magicNumbers
+
+    d = False #initiate d with False, so that d has to be found in order to return the variables required
+    while d == False:
+        randomNumber = int(random.random() * 1000)
+        d = dCheck(relativePrimes[randomNumber], oN)
+        if d != False: #When d is found, we can return the values needed for encryption and decryption
+            return relativePrimes, oN, n, d, int(relativePrimes[randomNumber]) #last value is e!
+
 
 
 #   --> Returns d if found
-#       D can be any number from the magicNumbers list, test some out.
-#       If dCheck returns a number from that magicNumbers list, you can use it for decryption
+#       d can be any number from the relativePrimes list, test some out.
+#       If dCheck returns a number from that relativePrimes list, you can use it for decryption
 #       oN is a fixed value calculated from (p - 1) (p - 1)
 #       e is any a magic number, find one that rules out 1!
 def dCheck(e, oN):
 
     i = 1
-    while True:
+    while i < 10000000:
         if((i * e) % oN == 1): #Check if e can be used for d too
-            print(i)
             return i
         i += 1
-        print((i * e) % oN, " || ", i)
+    return False
 
+#   --> Encrypt payload with public key
+def encrypt(e, n, payload):
+    return((payload**e)%n)
+
+#   --> Decrypt payload with private key
+def decrypt(d, n, payload):
+    return((payload**d)%n)
 
 
 def main():
@@ -154,9 +166,9 @@ def main():
 
 
 
-    x = findCoPrime(41,61) #p and q
-    print(x)
-    p = dCheck(23,2400)
+    relativePrimes, oN, n, d, e = RSA(41,61) #p and q
+    print("RelativePrimes: ",relativePrimes,"\noN: ", oN,"n: ", n,"d: ", d,"e: ", e,"\n")
+    p = dCheck(2093,oN) #check if e and oN have a relative prime, returns d if found
     print(p)
 
 
