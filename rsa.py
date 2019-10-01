70b942a8454a63677b35826672ee7a6428ade0da
SirThana <martijn.zijl.3000@gmail.com>
2019-10-01 21:51:41 +0200

    even more cleaning

--- rsa.py
+++ rsa.py
@@ -8,91 +8,30 @@ import binascii
 
 #TODO
 #   1. Do a binary search oN the primeList for more efficiency, lower runtime || doNe 
-#   2. Fix the most retarded algorithm every invented ||  working oN that
+#   2. Fix the most retarded algorithm every invented ||  Done
 #   3. Fix a case for greater than in binaryAlgorithm || doNe
-#   4. Determine scenario where x not in z, return False
+#   4. Determine scenario where x not in z, return False || Done
 #   5. Fix binary algo, i need to get 2 general while cases.
 #      while between 0 and 50 is true, half middle and stay in that 0 | 50 range
 #      Other way around while between 50 and 100,
-#      increase  the middle and stay in that 50 | 100 rang
+#      increase  the middle and stay in that 50 | 100 rang || Done
 #   6. binaryAlgorithm needs a False case || DONE
 #   7. Magic number calculation doesn't work, figure it out || you could give random magic numbers
-#       Until one works, if it works, the number returned will be d! and the number used e!
+#       Until one works, if it works, the number returned will be d! and the number used e! || DONE
 
 
-
-#Global primeList, stores prime numbers
-primeList = []
-testList = []
-#   --> Create a dummy list to envoke binary searches oN
-for c in range(1, 1000):
-    testList.append(c)
-
-#   --> Recursive binary search.
-#       Using recursioN, you exclude the other half oN every functioN call.
-#       z: list | x: value to find | lv: 0 left value | rv: list inversed [-1] | lastValue: 0
-#       Return True if x occurs in z
-def binaryAlgorithm(z, x, lv, rv, lastValue):
-    mid = int((lv + rv) // 2)
-    #If lastValue, which is the mid value from the previous recursioN
-    #is equal to the mid value calculated from the current recursioN 
-    #return False, because we're now stuck in dead search space
-    if lastValue == mid:
-        return False
-
-    #Value found, return True
-    if z[mid] == x:
-        return True
-
-    #Discard the right side of the search space
-    elif x < z[mid]:
-        print(mid, "|| Searching...")
-        return binaryAlgorithm(z, x, lv, mid, mid)
-
-    #Discard the left side of the search space
-    elif x > z[mid]:
-        print(mid, "|| Searching...")
-        return binaryAlgorithm(z, x, mid, rv, mid)
-
-
-#   --> Return true if x is a prime,
-#       Return false if x is not a prime OR if it already exists in z
-def isPrime(x):
-    #DoN't bother iterating if x is already in primeList
-    try:
-        if(binaryAlgorithm(primeList, x, 0, len(primeList), x) == True):
-            return False
-    except IndexError as e: #Out of bounds, the list begins empty
-        pass
-
-    if x == 0 or x == 1:
-        return False
-    if x == 2 or x == 3:
-        return True
-
-    else:
-        #If x module num is possible, then there is another common
-        #Divider among x, making it not a prime number
-        for num in range(2, x):
-            if x % num == 0:
-                return False
-
-    print("Prime Found")
-    return True
-
-#   --> Find the relative primes for e and d, using the two large prime numbers
-#       Returns a list of (most) relative numbers, some don't work!, test these by finding
-#       if they rule out 1 with dCheck
+#   --> Find the relative primes for e, test a random relative prime for d, with dCheck
+#       p & q are prime numbers, returns n, oN, relativePrime, e, & d 
 def RSA(p, q):
     n = p * q #Calculate the product of two primes
-    oN = ((p - 1) * (q - 1)) #used for eulers theorem || range of common dividers
+    oN = ((p - 1) * (q - 1)) #find a number that is relatively prime to this number
     relativePrimes = [] #List of numbers to test for eulers theorem
 
-    #Fix this retarded structure asap please || finds any relative primes
-    #Appends the relative numbers to a list called relativePrimes
+    #Appends the relative primes to a list called relativePrimes
     for e in range (2, oN):
-        print("Working out the possible relative Primes...", (e / oN) * 100)
+        print("Working out the possible relative Primes...", int((e / oN) * 100), "%")
         checkList = []
+
         result = e / n
         if result.is_integer() == True:
             checkList.append(False)
@@ -117,13 +56,11 @@ def RSA(p, q):
         else:
             checkList.append(True)
 
-
         if all(checkList): #If all elements are True, e is a relativePrime, save it
             relativePrimes.append(e)
-        #if checkList[0] == True and checkList[1] == True and checkList[2] == True and checkList[3] == True:
-        #    relativePrimes.append(e)
 
 
+    #Check if a random relative prime, is eligible to be the secret value d aswell
     relativePrimes.sort()
     d = False #initiate d with False, so that d has to be found in order to return the variables required
     while d == False:
@@ -137,14 +74,15 @@ def RSA(p, q):
 #   --> Returns d if found
 #       d is any a value between 1 and the biggest relativePrime, that rule out 1 with eulers theorem
 #       oN is a fixed value calculated from (p - 1) (p - 1), used in eulers theorem MODULO
-#       e is a random, relative prime, Relative to p & q 
+#       e is a random, relative prime, Relative to p & q
+#       condition is the biggest relativePrime found, you don't need to look past this prime
 def dCheck(e, oN, condition):
 
-    i = 1
-    while i < condition: #fix this by looping through the biggest number in relativePrimes list
-        if((i * e) % oN == 1): #Check if e can be used for d too
-            return i
-        i += 1
+    d = 1
+    while d < condition: #Iterate untill the biggest relativePrime relativePrime[-1]
+        if((d * e) % oN == 1): #Check if i is in common with e
+            return d
+        d += 1
     return False
 
 #   --> Encrypt payload with public key
@@ -158,38 +96,13 @@ def decrypt(d, n, payload):
 
 def main():
 
-    lastTime = datetime.datetime.now() #Keep track of time to meassure time between primes
-    for c in testList: #Iterate i times, trying to find primes i times in the range of x
-        x = int(random.random() * 10001)
-        if isPrime(x) == True:
-            print(datetime.datetime.now() - lastTime, " || " ,len(primeList))
-            lastTime = datetime.datetime.now()
-            primeList.append(x)
-            primeList.sort()
-    print(primeList, "\n")
-
-
-
-    relativePrimes, oN, n, d, e = RSA(19,11) #p and q
-    print("RelativePrimes: ",relativePrimes,"\noN: ", oN,"n: ", n,"d: ", d,"e: ", e,"\n")
-    payload = 2
+    relativePrimes, oN, n, d, e = RSA(1733,1301) #p and q
+    print(relativePrimes," << these are Relative Primes!\noN: ", oN,"n: ", n,"d: ", d,"e: ", e,"\n")
+    payload = 2999
     print("Starting value" ,payload)
     payload = encrypt(e, n, payload)
     print("Encrypted: ",payload)
     payload = decrypt(d, n, payload)
     print("Decrypted",payload)
-
-
-    #x = "guerangioer323"
-    #x = binascii.hexlify(x.encode())
-    #print(x)
-
-
-
-
-
-
-
-
 
 main()
